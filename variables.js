163888f3acaee302ca36351693873970e0cad4c0
AlokSinghTomar100 <aloktomar100303@gmail.com>
2026-01-07 23:05:21 +0530

    meghuuuu

---
+++ variables.js
@@ -0,0 +1,17 @@
+const accountId = 14453
+let accountEmail = "h@gmail.con"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+/* Prefer Not to use Var */
+
+// accountId = 2 not allowed
+
+accountEmail = "sdf@ff.com"
+accountPassword = "1234567890"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState ])
